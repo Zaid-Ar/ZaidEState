7ecfc8d3b85108320a90d288e6f1d7961d8a4c2b
Zaid Hamid <zaidhamid600@gmail.com>
2023-11-30 15:34:30 +0300

    adding bcrypt

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config()
 const app = express()
+app.use(express.json())
 const userRoutes = require("./routes/user")
-
-
+const authRoutes = require("./routes/auth")
+app.use("/api/auth", authRoutes);
 app.use('/api/user', userRoutes);
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to Mongodb")
 })

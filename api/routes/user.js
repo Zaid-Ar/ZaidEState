69b25f08930dae608a6e27848f8041349361e944
Zaid Hamid <zaidhamid600@gmail.com>
2023-12-07 20:18:58 +0300

    Revert "create  getUser Listing"
    This reverts commit 8bd90e34a909c1dc973b74564dd4223ea16a4311.

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,7 +3,6 @@ import {
   test,
   updateUser,
   deleteUser,
-  getUserListing,
 } from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,5 +11,5 @@ const router = express.Router();
 router.get("/", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listings/:id",verifyToken,getUserListing)
+
 export default router;

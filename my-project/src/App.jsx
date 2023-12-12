e6f610559fa465a596f1d8408090f81d2bd45c2c
Zaid Hamid <zaidhamid600@gmail.com>
2023-12-12 20:10:02 +0300

    adding prakets

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home"
-import SignIn from "./Pages/SignIn"
-import SignUp from "./Pages/SignUp"
-import About from "./Pages/About"
-import Profile from "./Pages/Profile"
-import Header from "./components/Header"
-import PrivateRoute from "./components/Private";
-import CreateListing from "./Pages/CreateListing";
-import Update from "./Pages/Update"
-import Listing from "./Pages/Listing";
-import Search from "./Pages/Search";
+import {Home} from "./Pages/Home"
+import {SignIn} from "./Pages/SignIn"
+import {SignUp} from "./Pages/SignUp"
+import {About} from "./Pages/About"
+import {Profile} from "./Pages/Profile"
+import {Header} from "./components/Header"
+import {PrivateRoute} from "./components/Private";
+import {CreateListing} from "./Pages/CreateListing";
+import {Update} from "./Pages/Update"
+import {Listing} from "./Pages/Listing";
+import {Search} from "./Pages/Search";
 
 export default function App() {
   return (

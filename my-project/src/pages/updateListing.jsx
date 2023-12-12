091df33b692bc5552d1447550b1628bd3f01f74a
Zaid Hamid <zaidhamid600@gmail.com>
2023-12-12 19:03:08 +0300

    change to create

--- my-project/src/pages/updateListing.jsx
+++ my-project/src/pages/updateListing.jsx
@@ -9,7 +9,7 @@ import { app } from "../firebase";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function UpdateListing() {
+export default function CreateListing() {
   const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const params = useParams();

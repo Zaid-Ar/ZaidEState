051cd14dae17a2380c79218162689920cde9e09b
Zaid Hamid <zaidhamid600@gmail.com>
2023-12-12 19:51:32 +0300

    Home problem

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./Pages/Home"
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
@@ -16,19 +16,19 @@ export default function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/search" element={<Search/>} />
+        <Route path="/listing/:listingId" element={<Listing/>} />
 
-        <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+        <Route element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/create-listing" element={<CreateListing/>} />
           <Route
             path="/update-listing/:listingId"
-            element={<UpdateListing />}
+            element={<UpdateListing/>}
           />
         </Route>
       </Routes>
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import {Swiper,SwiperSlide} from 'swiper/react'
-import 'swiper/css/bundle'
-import SwiperCore from 'swiper'
-import {Navigation} from 'swiper/modules'
-import ListingItem from '../components/ListingItem'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+import ListingItem from "../components/ListingItem";
 
 export default function Home() {
-    const [offerListings, setOfferListings] = useState([]);
-    const [saleListings, setSaleListings] = useState([]);
-    const [rentListings, setRentListings] = useState([]);
-    SwiperCore.use([Navigation])
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  console.log(offerListings);
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
@@ -44,24 +45,30 @@ export default function Home() {
     };
     fetchOfferListings();
   }, []);
-   
-
   return (
     <div>
-      <div className="flex flex-col  gap-6 p-28 px-3  max-w6xl mx-auto">
-        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl ">
-          Find your Next <span>Perfect</span> place
+      {/* top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
         </h1>
-        <div className="">
-          Zaid Estate is the best place to find your next perfect place to live
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Sahand Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
         </div>
         <Link
           to={"/search"}
           className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
         >
-          Let's get started
+          Let's get started...
         </Link>
       </div>
+
+      {/* swiper */}
       <Swiper navigation>
         {offerListings &&
           offerListings.length > 0 &&
@@ -78,16 +85,19 @@ export default function Home() {
             </SwiperSlide>
           ))}
       </Swiper>
+
+      {/* listing results for offer, sale and rent */}
+
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
           <div className="">
-            <div className="">
+            <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent offers
               </h2>
               <Link
                 className="text-sm text-blue-800 hover:underline"
-                to={`/search/offer=true`}
+                to={"/search?offer=true"}
               >
                 Show more offers
               </Link>
@@ -101,13 +111,13 @@ export default function Home() {
         )}
         {rentListings && rentListings.length > 0 && (
           <div className="">
-            <div className="">
+            <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent places for rent
               </h2>
               <Link
                 className="text-sm text-blue-800 hover:underline"
-                to={`/search?type=rent`}
+                to={"/search?type=rent"}
               >
                 Show more places for rent
               </Link>
@@ -121,19 +131,19 @@ export default function Home() {
         )}
         {saleListings && saleListings.length > 0 && (
           <div className="">
-            <div className="">
+            <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent places for sale
               </h2>
               <Link
                 className="text-sm text-blue-800 hover:underline"
-                to={`/search?type=sale`}
+                to={"/search?type=sale"}
               >
-                Show more places for rent
+                Show more places for sale
               </Link>
             </div>
             <div className="flex flex-wrap gap-4">
-              {offerListings.map((listing) => (
+              {saleListings.map((listing) => (
                 <ListingItem listing={listing} key={listing._id} />
               ))}
             </div>

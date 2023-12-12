e73bea36e57716b43d53c2a424ea4906825aaff7
Zaid Hamid <zaidhamid600@gmail.com>
2023-12-12 14:48:05 +0300

    add deploy

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -26,7 +26,9 @@ export default function Header() {
       <div className="bg-slate-200">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
           <Link to="/">
-            <h1 className="font-bold">ZaidEstate</h1>
+            {" "}
+            <span className="text-slate-500 text-2xl">Zaid</span>
+            <span className="text-slate-700 text-2xl">Estate</span>
           </Link>
           <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg">
             <input

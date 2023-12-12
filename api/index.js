db5747283cc8853981752854a38806545e755eb1
Zaid Hamid <zaidhamid600@gmail.com>
2023-12-12 16:18:44 +0300

    after mess

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRoutes from "./routes/user.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.js";
-// import path from "path";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -17,15 +17,15 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
